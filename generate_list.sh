218ea1b027fa3dc64444018038fe5b189d869cb3
Jan Jongerden <jan.jongerden@gmail.com>
2022-04-10 19:14:21 +0200

    Make sure subsub dir exists

--- generate_list.sh
+++ generate_list.sh
@@ -6,6 +6,8 @@ curl --compressed https://www.rijksoverheid.nl/onderwerpen > onderwerpen.html
 javac FilterSubjectUrls.java
 java FilterSubjectUrls > url_list.txt
 
+mkdir -p subsubjects
+
 cat url_list.txt | while read line ;
 do
   sleep 5
