80cc3c9997e33c55ad89dbc1b9251f49ee13418f
Jan Jongerden <jan.jongerden@gmail.com>
2023-03-24 16:52:33 +0100

    Improve download script
    - use curl silently to prevent verbose cron logs
    - fix some bash warnings

--- generate_list.sh
+++ generate_list.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 set -e
 
-curl --compressed https://www.rijksoverheid.nl/onderwerpen > onderwerpen.html
+curl --silent --compressed https://www.rijksoverheid.nl/onderwerpen > onderwerpen.html
 
-javac *.java
+javac ./*.java
 
 java FilterSubjectUrls > url_list.txt
 
 mkdir -p subsubjects
 
-cat url_list.txt | while read line ;
+cat url_list.txt | while read -r line ;
 do
   sleep 5
-  subsub=subsubjects/$(echo $line | cut -c42-).html
-  echo fetching: $line
-  curl --compressed $line > $subsub
+  subsub=subsubjects/$(echo "$line" | cut -c42-).html
+#  echo fetching: "$line"
+  curl --silent --compressed "$line" > "$subsub"
 done
 
 java GenerateSubjectsHtml > index.html
